52b4e48529d732ea5850ca544e2ea6b80be72155
dmoulart <dorian@resalocal.com>
2022-01-20 22:38:51 +0100

    Add navbar texts

--- components/organisms/NavBar.vue
+++ components/organisms/NavBar.vue
@@ -1,11 +1,68 @@
 <template>
   <m-navbar class="main-navbar" :class="{'navbar--extends': isHovered}" @hover="slide">
-    <m-navbutton icon="nav/home" />
-    <m-navbutton icon="nav/booking" />
-    <m-navbutton icon="nav/activity" />
-    <m-navbutton icon="nav/vehicle" />
-    <m-navbutton icon="nav/expanse" />
-    <div v-if="!$device.isMobile" class="main-navbar__panel" :class="{'main-navbar__panel--slidein': isHovered, 'main-navbar__panel--slideout': mustSlideout}" />
+    <m-navbutton class="main-navbar__button" icon="nav/home">
+      <span
+        class="main-navbar__item"
+        :class="{
+          'main-navbar__item--fadein': isHovered,
+          'main-navbar__item--fadeout': mustSlideout
+        }"
+      >
+        Accueil
+      </span>
+    </m-navbutton>
+    <m-navbutton class="main-navbar__button" icon="nav/booking">
+      <span
+        class="main-navbar__item"
+        :class="{
+          'main-navbar__item--fadein': isHovered,
+          'main-navbar__item--fadeout': mustSlideout
+        }"
+      >
+        Réservations
+      </span>
+    </m-navbutton>
+    <m-navbutton class="main-navbar__button" icon="nav/activity">
+      <span
+        class="main-navbar__item"
+        :class="{
+          'main-navbar__item--fadein': isHovered,
+          'main-navbar__item--fadeout': mustSlideout
+        }"
+      >
+        Activités
+      </span>
+    </m-navbutton>
+    <m-navbutton class="main-navbar__button" icon="nav/vehicle">
+      <span
+        class="main-navbar__item"
+        :class="{
+          'main-navbar__item--fadein': isHovered,
+          'main-navbar__item--fadeout': mustSlideout
+        }"
+      >
+        Véhicules
+      </span>
+    </m-navbutton>
+    <m-navbutton class="main-navbar__button" icon="nav/expanse">
+      <span
+        class="main-navbar__item"
+        :class="{
+          'main-navbar__item--fadein': isHovered,
+          'main-navbar__item--fadeout': mustSlideout
+        }"
+      >
+        Dépenses
+      </span>
+    </m-navbutton>
+    <div
+      v-if="!$device.isMobile"
+      class="main-navbar__panel"
+      :class="{
+        'main-navbar__panel--slidein': isHovered,
+        'main-navbar__panel--slideout': mustSlideout
+      }"
+    />
   </m-navbar>
 </template>
 
@@ -26,30 +83,70 @@ export default class NavBar extends MVue {
 </script>
 
 <style lang="scss" scoped>
-$developedSideBarWidth: 320px;
+$developedSideBarWidth: 200px;
+$slideTime : 0.3s;
+$nav-item-text-delay: $slideTime/2;
+$nav-item-text-margin: 24px;
+
 .main-navbar{
+  position: relative;
     &__panel{
-        position: absolute;
-        left: -$developedSideBarWidth;
-        top: 0;
-        z-index: -1;
-        height: 100%;
-        width: $developedSideBarWidth;
-        background-color: $white;
-        &--slidein{
-            animation: 0.3s slidein ease-out;
-            left:0;
+      position: absolute;
+      left: -$developedSideBarWidth;
+      top: 0;
+      z-index: -1;
+      height: 100%;
+      width: $developedSideBarWidth;
+      background-color: $white;
+      &--slidein{
+          animation: $slideTime slidein ease-out;
+          left:0;
+      }
+      &--slideout{
+          animation: $slideTime slideout ease-in ;
+          left:-$developedSideBarWidth;
+      }
+    }
+    &__button{
+      position: relative;
+      display:flex;
+      align-items: center;
+    }
+    &__item{
+      display:none;
+      @include tablet-portrait {
+        display: block;
+        opacity: 0;
+        position:absolute;
+        padding: 8px;
+        text-align: center;
+        font-size: 1.2rem;
+        margin-left: $nav-item-text-margin;
+        &:hover{
+          color: $black;
         }
-        &--slideout{
-            animation: 0.3s slideout ease-in;
-            left:-$developedSideBarWidth;
+        &--fadein{
+          opacity: 1;
+          animation: $slideTime*2 fadein ease-out;
         }
+        &--fadeout{
+          opacity: 0;
+          animation: $slideTime fadeout ease-in;
+        }
+      }
     }
 }
+// Todo : refactor by reversing animations
 @keyframes slidein {
     from { left:-$developedSideBarWidth; } to { left: 0; }
 }
 @keyframes slideout {
     from { left: 0; } to { left:-$developedSideBarWidth; }
+}
+@keyframes fadein {
+    from { opacity: 0; } to { opacity: 1; }
+}
+@keyframes fadeout {
+    from { opacity: 1; } to { opacity: 0; }
 }
 </style>
